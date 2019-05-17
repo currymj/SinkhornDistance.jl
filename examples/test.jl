a32ee09a96fb7597dd8faa6853025ce2b6097d0c
Michael J. Curry <curry@cs.umd.edu>
2019-05-17 18:24:07 -0400

    rescale test btwn 0 and 1

--- examples/test.jl
+++ examples/test.jl
@@ -3,13 +3,14 @@ using Distributions
 
 function example_data(n)
     distmat = zeros(n,n)
-    marginal_a = [exp(-(x - n/4)^2/8) for x=1:n]
+    marginal_a = [exp(-(x - n/2)^2/8) for x=1:n]
     marginal_b = [exp(-(x - 3n/4)^2/8) for x=1:n]
+    marginal_b .+= [exp(-(x - n/4)^2/8) for x=1:n]
     marginal_a ./= sum(marginal_a)
     marginal_b ./= sum(marginal_b)
     for i=1:n
         for j=1:n
-            distmat[i, j] = abs(i-j)
+            distmat[i, j] = abs((i/n)-(j/n))
         end
     end
     (marginal_a, marginal_b, distmat)
